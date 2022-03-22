4dd29f835825b6897a42981a2579d422747da627
Андрей Кузьменко <triplefork88@gmail.com>
2022-03-22 16:18:20 +0300

    changed post to get

--- authserver/src/main/java/com/microservicetest/authserver/controller/TokenAPI.java
+++ authserver/src/main/java/com/microservicetest/authserver/controller/TokenAPI.java
@@ -1,6 +1,5 @@
 package com.microservicetest.authserver.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microservicetest.authserver.domain.AppUser;
 import com.microservicetest.authserver.domain.TokenEntity;
 import com.microservicetest.authserver.service.TokenService;
@@ -8,16 +7,12 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
